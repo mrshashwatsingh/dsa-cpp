76df0a594e2a3453b2b9218c4ecbea60a1243218
Shashwat Singh <shashwatsingh5381@gmail.com>
2025-11-18 12:00:47 +0530

    Single Number solved on LeetCode.

---
+++ leetcode/Q136.cpp
@@ -0,0 +1,13 @@
+/*
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num=0;
+        for(size_t i=0; i<nums.size(); i++) {
+            num ^= nums[i];
+        }
+
+        return num;
+    }
+};
+*/
