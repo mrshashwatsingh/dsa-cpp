551a7ba485c29df066cb3b5dcc4c3469f9f20a40
Shashwat Singh <shashwatsingh5381@gmail.com>
2025-11-18 12:08:07 +0530

    Added explaination to Problem 136

--- leetcode/Q136.cpp
+++ leetcode/Q136.cpp
@@ -1,10 +1,26 @@
 /*
+Used properties XOR to solve the problem.
+
+Since,  a^0 == a;
+        a^a == 0;
+        a^b == b^a;
+        (a^b)^c == a^(b^c);
+
+We will XOR all the elements of the array and only the element that has unique value will be left.
+
+    n1^n2^n3^...^n1^n2^n3^...^n
+=>  (n1^n1)^(n2^n2)^(n3^n3)^...^n
+=>  0^0^0^...^n
+=>  n (unique value)
+*/
+
+/*
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int num=0;
-        for(size_t i=0; i<nums.size(); i++) {
-            num ^= nums[i];
+        for(int e : nums) {
+            num ^= e;
         }
 
         return num;
